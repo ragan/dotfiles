33f75f5773fc6be105dc319d93a16b346fdf15da
Karol Peek <peek.karol@gmail.com>
2025-01-07 19:54:29 +0100

    chore: Remove unnecessary comments and dependencies from neotree config

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -11,12 +11,3 @@ return {
 		vim.api.nvim_set_keymap('n', '<C-n>', ':Neotree left<CR>', { noremap = true, silent = true })
 	end
 }
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-	}
-}
