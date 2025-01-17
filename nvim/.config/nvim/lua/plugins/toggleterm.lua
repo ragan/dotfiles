0eee4e5ae295db16839f4f840a5b039ec298f0c0
Karol Peek <peek.karol@gmail.com>
2025-01-17 19:41:24 +0100

    toggleterm floating window

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -1,11 +1,13 @@
 return {
 	{
-		{
-			'akinsho/toggleterm.nvim',
-			version = "*",
-			opts = {
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("toggleterm").setup({
 				auto_scroll = true,
-			},
-		}
-	}
+				direction = "float",
+			})
+			vim.api.nvim_set_keymap("n", "<C-t>", ":ToggleTerm<CR>", { noremap = true, silent = true })
+		end,
+	},
 }
