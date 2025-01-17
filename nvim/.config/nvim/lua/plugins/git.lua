ff362b00d5dfa86ae3284789ee376326a3c583bb
Karol Peek <peek.karol@gmail.com>
2025-01-17 19:51:43 +0100

    git stuff

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -6,4 +6,19 @@ return {
 			{ "<leader>gs", ":Git<CR>", desc = "Git status" },
 		},
 	},
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "BufRead",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
+	{
+		"sindrets/diffview.nvim",
+		cmd = "DiffviewOpen",
+		keys = {
+			{ "<leader>gd", ":DiffviewOpen<CR>", desc = "Open diff view" },
+		},
+		config = true,
+	},
 }
