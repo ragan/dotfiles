c340f89432d2bb2a1c2975cf08b01ee26abbb30b
Karol Peek <peek.karol@gmail.com>
2025-01-18 09:52:35 +0100

    black

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -3,20 +3,20 @@ return {
 		"williamboman/mason.nvim",
 		config = function()
 			require("mason").setup()
-		end
+		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
-			require("mason-lspconfig").setup( {
+			require("mason-lspconfig").setup({
 				ensure_installed = { "lua_ls", "rust_analyzer", "gopls", "pyright" },
 			})
-		end
+		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({})
 			lspconfig.gopls.setup({
@@ -25,21 +25,21 @@ return {
 			lspconfig.pyright.setup({
 				capabilities = capabilities,
 			})
-			local bufopts = { noremap=true, silent=true, buffer=bufnr }
-			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-			vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-			vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-			vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-			vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-			vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-			vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-			vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-			vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-			vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-		end
+			local bufopts = { noremap = true, silent = true, buffer = bufnr }
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+			vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+			vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+			vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+			vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+		end,
 	},
 	{
 		"simrat39/rust-tools.nvim",
-	}
+	},
 }
--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -6,6 +6,7 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.gofumpt,
+				null_ls.builtins.formatting.black,
 			},
 		})
 
