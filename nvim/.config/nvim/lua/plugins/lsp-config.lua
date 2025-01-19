6feb0711e4713a44586d8f091db81a8a19be57b6
Karol Peek <peek.karol@gmail.com>
2025-01-19 10:51:36 +0100

    nvim java clang

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "rust_analyzer", "gopls", "pyright" },
+				ensure_installed = { "lua_ls", "rust_analyzer", "gopls", "pyright", "jdtls", "clangd" },
 			})
 		end,
 	},
@@ -23,6 +23,12 @@ return {
 				capabilities = capabilities,
 			})
 			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.jdtls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.clangd.setup({
 				capabilities = capabilities,
 			})
 			local bufopts = { noremap = true, silent = true, buffer = bufnr }
