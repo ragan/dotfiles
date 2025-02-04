3af0a25aeadf1caa072f52f9e075f33fda1b9022
ragan <peek.karol@gmail.com>
2025-02-04 07:06:51 +0100

    polybar

--- polybar/.config/polybar/launch_polybar.sh
+++ polybar/.config/polybar/launch_polybar.sh
@@ -1,7 +1,16 @@
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload toph &
-  done
-else
-  polybar --reload toph &
-fi
+#!/usr/bin/env sh
+
+# See: https://www.reddit.com/r/i3wm/comments/6lo0z0/how_to_use_polybar/
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+PRIMARY = $(polybar -m | grep "primary" | cut -d":" -f1)
+
+MONITOR=$PRIMARY polybar --reload primary &
+
+for m in $(polybar -m | grep -v "primary" | cut -d":" -f1); do
+	MONITOR=$m polybar --reload toph &
+done
